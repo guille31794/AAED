d467f7eecc8333731e98859c1ef0fe4c3f9ae0c6
guille31794 <guille.g94@hotmail.es>
2018-08-05 19:35:23 +0200

    Bateria de pruebas del ejercicio 7 terminada, y ejercicio funcionando perfectamente

--- Practica 7/ejercicio7.cpp
+++ Practica 7/ejercicio7.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 #include "listaDoble.hpp"
 
 using namespace std;
@@ -9,16 +11,73 @@ template <typename T> Lista<T> concatenate(const Lista<Lista<T>>& LInic)
 
   typename Lista<Lista<T>>::posicion pLL = LInic.primera();
   typename Lista<T>::posicion pLT = LInic.elemento(pLL).primera();
+  typename Lista<T>::posicion pLC = LConcat.primera();
 
-  for(pLL; pLL != LInic.fin(); pLL = LInic.siguiente(pLL))
-    for(pLT; pLT != LInic.elemento(pLL).fin(); pLT = LInic.elemento(pLL).siguiente(pLT))
-      LConcat.insertar(LInic.elemento(pLL).elemento(pLT));
+  while(pLL != LInic.fin())
+  {
+    while(pLT != LInic.elemento(pLL).fin())
+    {
+      LConcat.insertar(LInic.elemento(pLL).elemento(pLT), pLC);
+      pLC = LConcat.siguiente(pLC);
+      pLT = LInic.elemento(pLL).siguiente(pLT);
+    }
+
+    pLL = LInic.siguiente(pLL);
+    if(pLL != LInic.fin())
+      pLT = LInic.elemento(pLL).primera();
+  }
+
 
   return LConcat;
 }
 
-int main(int argc, char const *argv[]) 
+void insertElement(Lista<unsigned>& L)
 {
+  for (int c = 0; c < 5; ++c)
+  {
+    Lista<unsigned>::posicion p = L.primera();
+    L.insertar((rand()%10 + 1), p);
+    p = L.siguiente(p);
+  }
+}
+
+void printList(const Lista<unsigned>& L)
+{
+  Lista<unsigned>::posicion p = L.primera();
+
+  while(p != L.fin())
+  {
+    cout << L.elemento(p) << ' ';
+    p = L.siguiente(p);
+  }
+
+  cout << endl;
+}
+
+void insertList(Lista<Lista<unsigned>>& L)
+{
+  Lista<Lista<unsigned>>::posicion pLL = L.primera();
+  Lista<unsigned> Lista;
+
+  for(int i = 0; i < 5; ++i)
+  {
+    L.insertar( Lista, pLL);
+    insertElement(L.elemento(pLL));
+    printList(L.elemento(pLL));
+    pLL = L.siguiente(pLL);
+  }
+}
+
+int main()
+{
+  srand(time(NULL));
+  Lista<Lista<unsigned>> LL;
+
+  insertList(LL);
+
+  Lista<unsigned> LC(concatenate(LL));
+
+  printList(LC);
 
   return 0;
 }
